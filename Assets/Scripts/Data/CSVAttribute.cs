108cb305cf65828414b5875d16cecd770fa92b87
le888 <707649841@qq.com>
2016-10-20 20:33:11 +0800

    csv数据读取

---
+++ Assets/Scripts/Data/CSVAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false,Inherited = true)]
+class CSVAttribute:Attribute
+{
+    bool isCSVData;
+    public CSVAttribute(bool isCSVData)
+    {
+        this.isCSVData = isCSVData;
+    }
+
+    public bool IsCSVData 
+    {
+        get
+        { return isCSVData; 
+        }
+    }
+}
+
---
+++ Assets/Scripts/Data/CSVData.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+class CSVData
+{
+    private static string[] tableTitle;
+    public static List<object> LoadData(string filePath,string typeName)
+    {
+        Type ty = typeof(int);
+        List<object> list = new List<object>();
+
+        using (StreamReader sr = new StreamReader(filePath,Encoding.Unicode))
+        {
+            string tempStr;
+
+            sr.ReadLine();//第一行标示行没用
+
+            tempStr = sr.ReadLine();//第二行 title 与类名对应,区分大小写
+            tableTitle = tempStr.Split(',');
+
+            while((tempStr = sr.ReadLine())!= null)
+            {
+                string[] strs = tempStr.Split(',');
+                list.Add(DataForType(strs, typeName));
+            }
+        }
+        return list;   
+    }
+
+    private static object DataForType(string[] data, string typeName)
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        
+
+        var target  = assembly.CreateInstance(typeName,false);
+
+        FieldInfo[] fieldInfos = target.GetType().GetFields();
+
+        for(int i =0;i<fieldInfos.Length-1;i++)
+        {
+            if (!IsCSVData(fieldInfos[i]))
+                continue;
+
+            object value = Matching(fieldInfos[i].Name, data, i);
+
+          if (value == null)
+              continue;
+
+          MatchTypeValue(target, fieldInfos[i], value);
+
+        }
+        return target;
+    }
+
+    private static void MatchTypeValue(object target, FieldInfo fieldInfo, object value)
+    {
+        if (fieldInfo.FieldType == typeof(int))
+        {
+            fieldInfo.SetValue(target, int.Parse(value.ToString()));
+
+        }
+        else if (fieldInfo.FieldType == typeof(float))
+        {
+            fieldInfo.SetValue(target, float.Parse(value.ToString()));
+        }
+        else if (fieldInfo.FieldType == typeof(double))
+        {
+            fieldInfo.SetValue(target, double.Parse(value.ToString()));
+        }
+        else if (fieldInfo.FieldType == typeof(string))
+        {
+            fieldInfo.SetValue(target, value.ToString());
+        }
+    }
+
+    private static bool IsCSVData(FieldInfo pInfo)
+    {
+        object[] customAttribus = pInfo.GetCustomAttributes(false);
+       foreach(object o in customAttribus)
+       {
+           if(o is CSVAttribute)
+           {
+               return (o as CSVAttribute).IsCSVData;
+           }
+           continue;
+       }
+        return false;
+    }
+
+    private static object Matching(string PropertesName, string[] data, int fistIndex)
+    {
+        if(PropertesName.Equals(tableTitle[fistIndex]))
+            return data[fistIndex];
+
+        int leng = tableTitle.Length - 1;
+        if (leng > data.Length - 1)
+            leng = data.Length - 1;
+
+        for(int i=0;i<leng;i++)
+        {
+            if(PropertesName.Equals(tableTitle[i]))
+                return data[i];
+        }
+        return null;
+    }
+}
