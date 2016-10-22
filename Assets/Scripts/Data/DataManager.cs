c5d87f621b3b90d9fde65154f0a9621fab6a2fb6
le888 <707649841@qq.com>
2016-10-22 16:03:17 +0800

    添加资源管理

---
+++ Assets/Scripts/Data/DataManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class DataManager
+{
+    private List<WindowsData> windowsData;
+
+    public List<WindowsData> WindowsData
+    {
+        get
+        {
+            return windowsData;
+        }
+
+        set
+        {
+            windowsData = value;
+        }
+    }
+
+
+
+
+    public void InitData()
+    {
+        //CSVData.LoadData( )
+    }
+}
+
---
+++ Assets/Scripts/Data/WindowsData.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WindowsData {
+
+    private int id;
+    private string name;
+    private string path;
+
+    public int Id
+    {
+        get
+        {
+            return id;
+        }
+
+        set
+        {
+            id = value;
+        }
+    }
+
+    public string Name
+    {
+        get
+        {
+            return name;
+        }
+
+        set
+        {
+            name = value;
+        }
+    }
+
+    public string Path
+    {
+        get
+        {
+            return path;
+        }
+
+        set
+        {
+            path = value;
+        }
+    }
+}
